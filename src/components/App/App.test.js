194893854a8d024f1288cf84e77419ce13d5076c
juanmndz <juanmndz19@gmail.com>
2018-08-16 02:06:26 -0700

    Add test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => (
-  <div>
-basic
-  </div>
-);
-
-export default App;
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
