deb2f7a3547106b80f5d280e58e50d6616ec298d
juanmndz <juanmndz19@gmail.com>
2018-08-16 08:20:50 -0700

    test

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = compose;
 const store = createStore(rootReducer,
     composeEnhancers(
         applyMiddleware(thunk),
