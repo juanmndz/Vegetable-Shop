1f16d534652f7a9e96cb40c23bf1eff9b6f9e8a0
juanmndz <juanmndz19@gmail.com>
2018-08-16 07:51:27 -0700

    Readd test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-describe('test', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
-})
+test('Link changes the class when hovered', () => {
+  const component = renderer.create(
+    <App />,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
